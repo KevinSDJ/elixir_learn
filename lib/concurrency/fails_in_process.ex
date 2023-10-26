18282ca8f2394824025e5de0db228162f679024e
kevin De Jesus <sebadjkevin@gmail.com>
2023-10-26 19:59:36 -0300

    concurrency: GenServer exercis

--- lib/concurrency/fails_in_process.ex
+++ lib/concurrency/fails_in_process.ex
@@ -8,8 +8,9 @@ defmodule Concurrency.FailsInProcess do
       {:+,pid,x,y} -> send(pid,x+y)
       {:-,pid,x,y} -> send(pid,x-y)
       {:x,pid,x,y} -> send(pid,x*y)
+      {:/,_pid,_x,0} -> exit("Error divided by zero")
+      {:/,_pid,0,_} -> exit("Error divided by zero")
       {:/,pid,x,y} -> send(pid,x/y)
-      _-> exit("")
     end
     calc()
   end
---
+++ lib/concurrency/genserv_demo.ex
@@ -0,0 +1,13 @@
+
+
+defmodule Concurrency.GenservDemo do
+
+  use GenServer
+
+  def start_link(state \\ []) do
+     GenServer.start(__MODULE__, state , name: __MODULE__)
+  end
+
+  def init(state), do: {:ok,state}
+
+end
